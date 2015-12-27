4f59d16af772ac924aae444262e7eac523e643c3
tkmagesh <tkmagesh77@gmail.com>
2015-12-27 14:06:12 +0530

    Promise

--- Day-01/01-AsyncProgramming/asyncDemo.js
+++ Day-01/01-AsyncProgramming/asyncDemo.js
@@ -46,3 +46,51 @@ function addAsyncClient(x,y){
         console.log("[SC] result = ", result);    
     });
 }
+
+//Provider
+function Adder(){
+    var _callbacks = [];
+
+    this.add = function addAsync(x,y,onResult){
+        console.log("[SP] processing ", x , " and ", y);
+        setTimeout(function(){
+            //if either x or y is (undefined | null | 0) throw error
+            if (!x || !y) {
+                var err = new Error("invalid arguments");
+                _callbacks.forEach(function(callback){
+                    callback(err);
+                });
+                return;
+            }
+            var result = x + y;
+            console.log("[SP] returning result");
+            //return result;
+            _callbacks.forEach(function(callback){
+                callback(null, result);
+            })
+        },3000);
+
+    };
+    this.onResult = function(callback){
+        _callbacks.push(callback);
+    }
+
+}
+
+
+//Promise
+function addAsyncUsingPromise(x,y){
+    console.log("[SP] processing ", x , " and ", y);
+    var promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            if (!x || !y){
+                reject("invalid arguments");
+            } else {
+                var result = x + y;
+                console.log("[SP] returning result");
+                resolve(result);
+            }
+        },3000);
+    });
+    return promise;
+}
