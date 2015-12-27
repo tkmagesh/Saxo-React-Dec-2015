c5953747daeba0ba06706f2e241b087907d83b58
tkmagesh <tkmagesh77@gmail.com>
2015-12-27 11:50:06 +0530

    Using "Error First" callbacks

--- Day-01/01-AsyncProgramming/asyncDemo.js
+++ Day-01/01-AsyncProgramming/asyncDemo.js
@@ -21,11 +21,15 @@ function addAsync(x,y,onResult){
     console.log("[SP] processing ", x , " and ", y);
     setTimeout(function(){
         //if either x or y is (undefined | null | 0) throw error
+        if (!x || !y) {
+            var err = new Error("invalid arguments");
+            return onResult(err, null);
+        }
         var result = x + y;
         console.log("[SP] returning result");
         //return result;    
         if (typeof onResult === 'function')
-            onResult(result);
+            onResult(null, result);
     },3000);
     
 }
@@ -34,7 +38,11 @@ function addAsync(x,y,onResult){
 //modify the below to handle the error and display a meaniful message
 function addAsyncClient(x,y){
     console.log("[SC] triggering add");
-    addAsync(x,y, function(result){
+    addAsync(x,y, function(err, result){
+        if (err){
+            console.log("error occured..", err);
+            return;
+        }
         console.log("[SC] result = ", result);    
     });
 }
