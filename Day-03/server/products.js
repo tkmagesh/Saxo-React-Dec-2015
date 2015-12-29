a64e77315873184588dd5ade0624f20d8ff0af71
tkmagesh <tkmagesh77@gmail.com>
2015-12-29 14:52:21 +0530

    json file for server

---
+++ Day-03/server/products.js
@@ -0,0 +1,7 @@
+{
+    "products" : [
+        {"id" : 1, "name" : "Pen", units : 10, cost : 50},
+        {"id" : 2, "name" : "Pencil", units : 20, cost : 30},
+        {"id" : 3, "name" : "Den", units : 30, cost : 10}
+    ]
+}
