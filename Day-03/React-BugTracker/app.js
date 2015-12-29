0bedefbf8561da0587a62f44c55d384242810cb0
tkmagesh <tkmagesh77@gmail.com>
2015-12-29 11:50:16 +0530

    repeating nodes

--- Day-03/React-BugTracker/app.js
+++ Day-03/React-BugTracker/app.js
@@ -5,16 +5,23 @@ var Greeter = React.createClass({
     onGreet : function(){
           console.log("Greet button is clicked");
           var name = this.refs.txtName.value;
+          var count = parseInt(this.refs.txtCount.value, 10);
           var greetMsg = 'Hi ' + name + ', Have a nice day!';
           this.setState({message : greetMsg});
+          this.setState({count : count});
         },
     render : function(){
         console.log('re rendering');
+        var messages = [];
+        for(var i=0; i<this.state.count;i++)
+            messages.push((<div> {this.state.message} </div>))
+
         return (
           <div>
               <input type="text" ref="txtName" />
+              <input type="text" ref="txtCount" />
               <input type="button" value="Greet" onClick={this.onGreet} id="btnGreet"/>
-              <div>{this.state.message}</div>
+                {messages}
           </div>
         );
     }
